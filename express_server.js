219bef550d4e1ad0ac8e9e468e90ea0e6b09ffaf
Mike <mbrie041@gmail.com>
2021-03-16 02:19:45 +0000

    Adding a Second Route and Template

--- express_server.js
+++ express_server.js
@@ -9,22 +9,28 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => { //message at root - provided (probably won't keep)
-  res.send("Hello!");
-});
-
-app.get("/urls.json", (req, res) => { //message at URLS.JSON - provided (probably won't keep)
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => { //message at /hello - provided (probably won't keep)
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
 });
 
 app.get("/urls", (req, res) => { //message at /urls
   const urlObject = { urls: urlDatabase };
-  res.render("urls_index", urlObject);
+  res.render("urls_index", urlObject); //displayed as a table in index
 });
+
+
+// app.get("/", (req, res) => { //message at root - provided (probably won't keep)
+//   res.send("Hello!");
+// });
+
+// app.get("/urls/:shortURL", (req, res) => { //message at URLS.JSON - provided (probably won't keep)
+//   res.json(urlDatabase);
+// });
+
+// app.get("/hello", (req, res) => { //message at /hello - provided (probably won't keep)
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
