1d65a3104b34f0dfc637a580d3c5893c02693170
Mike <mbrie041@gmail.com>
2021-03-16 01:47:49 +0000

    Filling Out the urls_index.ejs Template

--- express_server.js
+++ express_server.js
@@ -9,16 +9,21 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
+app.get("/", (req, res) => { //message at root - provided (probably won't keep)
   res.send("Hello!");
 });
 
-app.get("/urls.json", (req, res) => {
+app.get("/urls.json", (req, res) => { //message at URLS.JSON - provided (probably won't keep)
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
+app.get("/hello", (req, res) => { //message at /hello - provided (probably won't keep)
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) => { //message at /urls
+  const urlObject = { urls: urlDatabase };
+  res.render("urls_index", urlObject);
 });
 
 app.listen(PORT, () => {
