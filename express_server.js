300a6302132c0bb6497db673e2283f301934c3e0
Mike <mbrie041@gmail.com>
2021-03-16 13:04:26 +0000

    Create a URL Submission Form

--- express_server.js
+++ express_server.js
@@ -2,21 +2,38 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-app.set("view engine", "ejs")
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true})); //body-parser library to read buffers
+
+app.set("view engine", "ejs") //reads EJS
+
+function generateRandomString() {
+  let randomShortUrl = 6;
+  return Math.random().toString(20).substr(2, randomShortUrl);  // function that returns a string of 6 random alphanumeric characters
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/urls", (req, res) => { //message at /urls
+  const urlObject = { urls: urlDatabase };
+  res.render("urls_index", urlObject); //displayed as a table in index
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new"); //get route to render the urls_new.ejs
+});
+
 app.get("/urls/:shortURL", (req, res) => { //displays short urls
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars); //rendering info about a single url in URL Show
 });
 
-app.get("/urls", (req, res) => { //message at /urls
-  const urlObject = { urls: urlDatabase };
-  res.render("urls_index", urlObject); //displayed as a table in index
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 
